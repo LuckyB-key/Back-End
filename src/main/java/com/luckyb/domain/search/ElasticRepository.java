223de12e64cbc0681ec84e74c41af88bba9886af
Hyun-jun-Lee0811 <korong0811@naver.com>
2025-08-23 01:14:41 +0900

    remove: elasticSearch 기능 삭제

--- src/main/java/com/luckyb/domain/search/ElasticRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luckyb.domain.search;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ElasticRepository extends ElasticsearchRepository<ShelterDocument, String> {
-
-}
